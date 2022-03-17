d3ee50f386509de74fae8ed0f231c3f241232a81
celenaal <101164218+celenaal@users.noreply.github.com>
2022-03-16 21:28:04 -0700

    updated image on js

--- script.js
+++ script.js
@@ -42,6 +42,11 @@ function showTemperature(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 function locationInput(event) {
   event.preventDefault();
