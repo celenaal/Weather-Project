100687097b956679b78f1a5ad50cba6dd548e0e4
celenaal <101164218+celenaal@users.noreply.github.com>
2022-03-23 21:24:34 -0700

    Removed temperature unit conversion

--- script.js
+++ script.js
@@ -115,34 +115,9 @@ function showPosition(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
 }
-function showFahrenheitTemp(event) {
-  event.preventDefault();
-  let showTemperature = document.querySelector("#main-tempt");
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = celsiusTemperature * (9 / 5) + 32;
-  showTemperature.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-function showcelsiusTemp(event) {
-  event.preventDefault();
-  let showTemperature = document.querySelector("#main-tempt");
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-
-  showTemperature.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
 
 let currentLoc = document.querySelector("#current-location");
 currentLoc.addEventListener("click", currentLocation);
-
-let fahrenheitLink = document.querySelector("#fahrenheit");
-fahrenheitLink.addEventListener("click", showFahrenheitTemp);
-
-let celsiusLink = document.querySelector("#celsius");
-celsiusLink.addEventListener("click", showcelsiusTemp);
 
 displayForecast();
 
